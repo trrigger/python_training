ca83454666d56f4a83f7c40137ad514ae98e3803
trrigger <tiggercake@gmail.com>
2017-06-11 17:59:17 +0300

    Улучшены функции login/logout

--- fixture/session.py
+++ fixture/session.py
@@ -3,6 +3,14 @@ class SessionHelper:
     def __init__(self, app):
         self.app = app
 
+    def is_logged_in(self):
+        wd = self.app.wd
+        return len(wd.find_elements_by_link_text("Logout")) > 0
+
+    def is_logged_in_as(self, username):
+        wd = self.app.wd
+        return wd.find_element_by_xpath("//div/div[1]/form/b").text == "("+username+")"
+
     def login(self, username, password):
         wd = self.app.wd
         self.app.open_home_page()
@@ -16,6 +24,20 @@ class SessionHelper:
         wd.find_element_by_id("LoginForm").click()
         wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
 
+    def ensure_login(self, username, password):
+        wd = self.app.wd
+        if self.is_logged_in():
+            if is_logged_in_as(username):
+                return
+            else:
+                self.logout()
+        self.login(username, password)
+
     def logout(self):
         wd = self.app.wd
         wd.find_element_by_link_text("Logout").click()
+
+    def ensure_logout(self):
+        wd = self.app.wd
+        if self.is_logged_in():
+            self.logout()
