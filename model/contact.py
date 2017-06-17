321d095ea36353bb816089b26f81bb2a53dd3276
trrigger <tiggercake@gmail.com>
2017-06-17 19:04:41 +0300

    Добавлена проверка равенства в тесты для удаления

--- model/contact.py
+++ model/contact.py
@@ -8,3 +8,9 @@ class Contact:
         self.mobile = mobile
         self.email = email
         self.id = id
+
+    def __repr__(self):
+        return "%s:%s" % (self.id, self.firstname)
+
+    def __eq__(self, other):
+        return self.id == other.id and self.firstname == other.firstname
