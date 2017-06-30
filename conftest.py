81d26eec36608191f7e4a498f433350b520a7c13
trrigger <tiggercake@gmail.com>
2017-06-30 17:07:35 +0300

    Реализованы опции командной строки

--- conftest.py
+++ conftest.py
@@ -6,20 +6,26 @@ fixture = None
 @pytest.fixture
 def app(request):
     global fixture
+    browser = request.config.getoption('--browser')
+    base_url = request.config.getoption('--baseUrl')
     if fixture is None:
-        fixture = Application()
+        fixture = Application(browser=browser, base_url=base_url)
     else:
         if not fixture.is_valid():
-            fixture = Application()
-            fixture.session.login(username="admin", password="secret")
+            fixture = Application(browser=browser, base_url=base_url)
     fixture.session.ensure_login(username="admin", password="secret")
     return fixture
 
 
-@pytest.fixture(scope = "session", autouse=True)
+@pytest.fixture(scope="session", autouse=True)
 def stop(request):
     def fin():
         fixture.session.ensure_logout()
         fixture.destroy()
     request.addfinalizer(fin)
     return fixture
+
+
+def pytest_addoption(parser):
+    parser.addoption('--browser', action='store', default='firefox')
+    parser.addoption('--baseUrl', action='store', default='http://localhost/addressbook/')
