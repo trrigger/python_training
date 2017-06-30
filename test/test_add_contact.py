998e8b9b22346a959a352bf4bf52d8c845ae62b6
trrigger <tiggercake@gmail.com>
2017-06-30 15:59:40 +0300

    Добавлена параметризация тестов

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,23 +1,26 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
+import pytest
+import random
+import string
 
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
 
-def test_add_contact(app):
-    old_contacts = app.contact.get_contact_list()
-    contact = Contact(firstname='Ivan', lastname='Petrov', title='engeneer',
-                      company='top labs', mobilephone='8-888-888-88-88', email='test@mail.ru')
-    app.contact.create(contact)
-    assert len(old_contacts) + 1 == app.contact.count()
-    new_contacts = app.contact.get_contact_list()
-    old_contacts.append(contact)
-    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
+testdata = [Contact(firstname='', lastname='', title='', company='', mobilephone='', email='test@mail.ru')] + [
+        Contact(firstname=random_string('firstname', 10), lastname=random_string('Petrov', 10), title=random_string('engeneer', 20),
+                company=random_string('top labs', 20), mobilephone=random_string('8-888-888-88-88', 30),
+                email=random_string('test@mail.ru', 30))
+for i in range(5)
+    ]
 
-
-# def test_add_empty_contact(app):
- #   old_contacts = app.contact.get_contact_list()
-#    contact = Contact(firstname='', lastname='', title='', company='', mobilephone='', email='test@mail.ru')
-#    app.contact.create(contact)
-#   new_contacts = app.contact.get_contact_list()
-#    assert len(old_contacts) + 1 == len(new_contacts)
-#    old_contacts.append(contact)
-#    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
+@pytest.mark.parametrize('contact', testdata, ids=[repr(x) for x in testdata])
+def test_add_contact(app, contact):
+        pass
+        old_contacts = app.contact.get_contact_list()
+        app.contact.create(contact)
+        assert len(old_contacts) + 1 == app.contact.count()
+        new_contacts = app.contact.get_contact_list()
+        old_contacts.append(contact)
+        assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
